1639ba055e6ac8c73fa4400a3e3757f014b542df
Cameron Williams <cameron@cameronvwilliams.com>
2018-06-11 13:39:27 -0700

    Require presenters call super in methods

--- app/src/main/java/com/cameronvwilliams/raise/ui/BasePresenter.kt
+++ app/src/main/java/com/cameronvwilliams/raise/ui/BasePresenter.kt
@@ -1,14 +1,17 @@
 package com.cameronvwilliams.raise.ui
 
 import io.reactivex.disposables.CompositeDisposable
+import javax.annotation.OverridingMethodsMustInvokeSuper
 
 abstract class BasePresenter {
     protected lateinit var viewSubscriptions: CompositeDisposable
 
+    @OverridingMethodsMustInvokeSuper
     open fun onViewCreated(v: BaseFragment) {
         viewSubscriptions = CompositeDisposable()
     }
 
+    @OverridingMethodsMustInvokeSuper
     open fun onViewDestroyed() {
         viewSubscriptions.clear()
     }
