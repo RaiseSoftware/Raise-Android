45040c927083f49f8a2c37100a6f10dfde808b26
Cameron Williams <cameron@cameronvwilliams.com>
2018-07-11 10:06:45 -0400

    Remove unused store classes

--- app/src/main/java/com/cameronvwilliams/raise/data/store/State.kt
+++
@@ -1,9 +0,0 @@
-package com.cameronvwilliams.raise.data.store
-
-import com.cameronvwilliams.raise.data.model.PokerGame
-
-abstract class State {
-
-    internal abstract fun pokerGame(): PokerGame
-
-}
